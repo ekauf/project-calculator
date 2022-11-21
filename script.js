d3753da3fec699b926f62febb4a327a3c4cad790
Edgars <Edgars.Kaufmanis@nology.io>
2022-11-21 16:51:16 +0000

    added clicked button to display

--- script.js
+++ script.js
@@ -2,20 +2,8 @@ const calculator = document.querySelector(".calculator");
 const buttons = Array.from(document.querySelectorAll(".calculator__button"));
 const display = document.querySelector(".calculator__display");
 
-// const handleButtonClick = Array.from.buttons.forEach((button) => {
-//   console.log(button.innerHTML);
-// });
-
-// buttons.forEach((button) => {
-//   console.log(button.innerHTML);
-// });
-
-// const handleButtonClick = (button) => {
-//   console.log(button.innerHTML);
-// };
-
 buttons.map((button) => {
   button.addEventListener("click", (buttonClick) => {
-    console.log(button.innerHTML);
+    display.innerHTML += button.innerHTML;
   });
 });
