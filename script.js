fbfe0981e12efdcf39536771e214fc72760ec390
Edgars <Edgars.Kaufmanis@nology.io>
2022-11-22 01:57:51 +0000

    night updates :(

--- script.js
+++ script.js
@@ -2,8 +2,26 @@ const calculator = document.querySelector(".calculator");
 const buttons = Array.from(document.querySelectorAll(".calculator__button"));
 const display = document.querySelector(".calculator__display");
 
-buttons.map((button) => {
-  button.addEventListener("click", (buttonClick) => {
-    display.innerHTML += button.innerHTML;
-  });
+const buttonClicked = buttons.map((button) => {
+  //   button.addEventListener("click", (buttonClick) => {
+  display.innerHTML += button.innerHTML;
+  //   });
 });
+
+const allClear = () => {
+  if (buttons[0]) {
+    display.innerHTML = " ";
+  }
+};
+
+// const action = buttonClicked.dataset.action;
+
+// if (!action) {
+//   console.log("number key");
+// }
+
+console.log(buttons);
+
+// buttons.addEventListener("click", allClear);
+
+buttons.addEventListener("click", buttonClicked);
