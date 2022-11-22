612efb025d8247baa4cf2029cae57c18697dc6f1
Edgars <Edgars.Kaufmanis@nology.io>
2022-11-22 09:02:37 +0000

    new approach, updated html and scss, added query selectors

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-const calculator = document.querySelector(".calculator");
-const buttons = Array.from(document.querySelectorAll(".calculator__button"));
-const display = document.querySelector(".calculator__display");
-
-const buttonClicked = buttons.map((button) => {
-  button.addEventListener("click", (buttonClick) => {
-    display.innerHTML += button.innerHTML;
-  });
-});
-
-const allClear = buttons.map((button) => {
-  button.addEventListener("click", (buttonClick) => {
-    if (button[0]);
-    display.innerHTML = " ";
-  });
-});
-
-// const action = buttonClicked.dataset.action;
-
-// if (!action) {
-//   console.log("number key");
-// }
-
-// console.log(buttons);
-
-// buttons.addEventListener("click", allClear);
-
-// buttons.addEventListener("click", buttonClicked);
+const numberButtons = document.querySelectorAll("[data-number");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const equalsButton = document.querySelector("[data-calculation]");
+const backspaceButton = document.querySelector("[data-backspace]");
+const negationButton = document.querySelector("[data-negation]");
+const percentageButton = document.querySelector("[data-percentage]");
+const previousOperandDisplay = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandDisplay = document.querySelector("[data-current-operand]");
